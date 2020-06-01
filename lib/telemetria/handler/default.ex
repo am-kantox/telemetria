307092c1ab9a740e4e141e0e825f7abf9d8b32f5
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2020-06-01 11:54:36 +0200

    Feature/#18 accept options in annotation (#19)
    * :paperclip: version bump
    
    * Closes #18

--- lib/telemetria/handler/default.ex
+++ lib/telemetria/handler/default.ex
@@ -16,8 +16,42 @@ defmodule Telemetria.Handler.Default do
   @doc false
   @impl Handler
   def handle_event(event, measurements, metadata, config) do
-    [event: event, measurements: measurements, metadata: metadata, config: config]
-    |> inspect()
-    |> Logger.info()
+    {inspect_opts, metadata} = Map.pop(metadata, :inspect, [])
+    {level, metadata} = pop_in(metadata, [:context, :level])
+    {result, metadata} = Map.pop(metadata, :result, :unknown)
+
+    level =
+      if is_nil(level),
+        do: Application.get_env(:telemetria, :level, :info),
+        else: level
+
+    inspected =
+      inspect(
+        [
+          event: event,
+          measurements: measurements,
+          result: result,
+          metadata: metadata,
+          config: config
+        ],
+        inspect_opts
+      )
+
+    do_log(level, inspected)
   end
+
+  @spec do_log(level :: Telemetria.Hooks.level(), message :: binary()) :: :ok
+  if(Version.match?(System.version(), ">= 1.11.0-dev")) do
+    @levels ~w|emergency alert critical error warn notice info debug|a
+  else
+    @levels ~w|error warn info debug|a
+  end
+
+  Enum.each(@levels, fn level ->
+    defp do_log(unquote(level), message) when is_binary(message),
+      do: Logger.unquote(level)(fn -> message end)
+  end)
+
+  defp do_log(level, message) when is_binary(message),
+    do: Logger.warn(fn -> "[#{inspect(level)}] " <> message end)
 end
