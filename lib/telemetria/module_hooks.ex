218b7ea4a63cd512783dbfedf70879aaca0b1cde
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2022-02-03 19:17:31 +0100

    Closed #27.

--- lib/telemetria/module_hooks.ex
+++ lib/telemetria/module_hooks.ex
@@ -26,51 +26,47 @@ defmodule Telemetria.Hooks do
   @typedoc false
   @type option :: {:level, level()} | {:inspect_opts, Inspect.Opts.t()} | {atom(), any()}
   @typedoc false
+  @type annotation_type :: :none | :head | :clause
+  @typedoc false
   @type info :: %{
+          annotation_type: annotation_type(),
           env: Macro.Env.t(),
           kind: :def | :defp,
           fun: atom(),
+          arity: arity(),
           args: [ast_tuple()],
           guards: [ast_tuple()],
           body: [{:do, ast_tuple()}],
           options: [option()]
         }
-  defstruct ~w|env kind fun args guards body options|a
+  defstruct ~w|annotation_type env kind fun arity args guards body options|a
 
   def __on_definition__(env, kind, fun, args, guards, body) do
     case {Module.get_attribute(env.module, :telemetria), kind, body} do
-      {nil, _, _} ->
-        :ok
+      {options, kind, body} when kind in [:def, :defp] ->
+        {type, options} = pop_apply(options, body)
 
-      {_, _, nil} ->
-        raise Telemetria.Error, "only functions with body can be currently annotated"
-
-      {_, kind, _} when kind not in [:def, :defp] ->
-        raise Telemetria.Error, "only function annotating is currently supported"
-
-      {options, kind, body} when is_list(options) or options == true ->
-        {apply?, options} = pop_apply(options)
-
-        if apply? do
-          Module.put_attribute(
-            env.module,
-            :telemetria_hooks,
-            struct(__MODULE__,
-              env: env,
-              kind: kind,
-              fun: fun,
-              args: args,
-              guards: guards,
-              body: body,
-              options: options
-            )
+        Module.put_attribute(
+          env.module,
+          :telemetria_hooks,
+          struct(__MODULE__,
+            annotation_type: type,
+            env: env,
+            kind: kind,
+            fun: fun,
+            arity: length(args),
+            args: args,
+            guards: guards,
+            body: body,
+            options: options
           )
-        end
+        )
 
         Module.delete_attribute(env.module, :telemetria)
 
-      {other, _, _} ->
-        raise Telemetria.Error, "inline handlers are not yet supported, #{inspect(other)} given"
+      {options, _kind, _body} when not is_nil(options) ->
+        raise Telemetria.Error,
+              "only function annotating is currently supported, please remove #{inspect(options)}"
     end
   end
 
@@ -79,6 +75,7 @@ defmodule Telemetria.Hooks do
       env.module
       |> Module.get_attribute(:telemetria_hooks)
       |> Enum.reverse()
+      |> fix_hooks()
 
     overrides =
       hooks
@@ -116,21 +113,80 @@ defmodule Telemetria.Hooks do
   defp maybe_guarded(guards, f, meta, args) when is_list(guards),
     do: {:when, [context: Elixir], [{f, meta, args} | guards]}
 
-  @spec pop_apply([{:if, boolean() | binary()} | option()]) :: {boolean(), [option()]}
-  defp pop_apply(true), do: pop_apply(if: true)
+  @spec pop_apply([{:if, boolean()} | option()] | boolean() | nil, Macro.t()) ::
+          {annotation_type(), [option()]}
+  defp pop_apply(nil, body), do: pop_apply(false, body)
+  defp pop_apply(false, body), do: pop_apply([if: false], body)
+  defp pop_apply(true, body), do: pop_apply([if: true], body)
 
-  defp pop_apply(options) do
+  defp pop_apply(options, body) do
     options
     |> Keyword.pop(:if, true)
     |> case do
-      {code, options} when is_binary(code) ->
-        {Code.compile_quoted(code), options}
+      {false, options} ->
+        {:none, options}
 
-      {apply?, options} when is_boolean(apply?) ->
-        {apply?, options}
+      {true, options} ->
+        {if(is_nil(body), do: :head, else: :clause), options}
 
       {weird, _options} ->
         raise Telemetria.Error, "unsupported `if` value " <> inspect(weird)
+    end
+  end
+
+  @spec fix_hooks([info()]) :: [info()]
+  defp fix_hooks(hooks) do
+    hooks
+    |> Enum.group_by(&{&1.fun, &1.arity})
+    |> Enum.flat_map(fn {{_fun, _arity}, hooks} ->
+      hooks
+      |> Enum.map(& &1.annotation_type)
+      |> Enum.uniq()
+      |> case do
+        [:head, :none] ->
+          [head | nones] = hooks
+          Enum.map(nones, &%__MODULE__{&1 | annotation_type: :clause, options: head.options})
+
+        [:head | _] ->
+          raise Telemetria.Error, "one cannot define both head and per-clause attributes"
+
+        [:clause] ->
+          hooks
+
+        [_] ->
+          []
+
+        _ ->
+          raise Telemetria.Error, "either a head clause, or all clauses, or none could be handled"
+      end
+    end)
+  end
+
+  defimpl Inspect do
+    @moduledoc false
+    import Inspect.Algebra
+
+    def inspect(hook, opts) do
+      args = hook.args |> Macro.to_string() |> String.slice(1..-2)
+
+      guards =
+        hook.guards
+        |> Macro.to_string()
+        |> String.slice(1..-2)
+        |> case do
+          "" -> ""
+          other -> " when " <> other
+        end
+
+      doc = [
+        annotation_type: hook.annotation_type,
+        fun: "#{hook.kind} #{hook.env.module}.#{hook.fun}(#{args})#{guards}",
+        arity: hook.arity,
+        body: not is_nil(hook.body),
+        options: hook.options
+      ]
+
+      concat(["#Telemetria<", to_doc(doc, opts), ">"])
     end
   end
 end
